fe703628eb80d6918d7a5a3b7c4aeff81ad7f358
Yelena Cox <yelenacox@gmail.com>
2025-05-22 12:43:19 -0500

    Started work on phenotype transforms

--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_demographics.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_demographics.sql
@@ -5,15 +5,21 @@
         GEN_UNKNOWN.date_of_birth::integer as "date_of_birth",
         GEN_UNKNOWN.date_of_birth_type::text as "date_of_birth_type",
         CASE participant.sex
-        WHEN 'Female' THEN 'female'
-        WHEN 'Male' THEN 'male'
-        WHEN 'Unknown' THEN 'unknown'
-        WHEN 'Intersex' THEN 'intersex'
-        ELSE participant.sex 
+            WHEN 'Female' THEN 'female'
+            WHEN 'Male' THEN 'male'
+            WHEN 'Unknown' THEN 'unknown'
+            WHEN 'Intersex' THEN 'intersex'
+            ELSE participant.sex 
         END::text as "sex",
         GEN_UNKNOWN.sex_display::text as "sex_display",
         participant.reported_race::text as "race_display",
-        participant.reported_ethnicity::text as "ethnicity",
+        CASE participant.reported_ethnicity
+            WHEN 'Hispanic or Latino' THEN 'hispanic_or_latino'
+            WHEN 'Not Hispanic or Latino' THEN 'not_hispanic_or_latino'
+            WHEN 'Unknown' THEN 'unknown'
+            WHEN 'asked but unknown' THEN 'asked_but_unknown'
+            ELSE participant.reported_ethnicity
+        END::text as "ethnicity",
         GEN_UNKNOWN.ethnicity_display::text as "ethnicity_display",
         GEN_UNKNOWN.age_at_last_vital_status::integer as "age_at_last_vital_status",
         GEN_UNKNOWN.vital_status::text as "vital_status",
--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_subjectassertion.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_subjectassertion.sql
@@ -3,6 +3,7 @@
     with source as (
         select 
         GEN_UNKNOWN.assertion_type::text as "assertion_type",
+<<<<<<< HEAD
        GEN_UNKNOWN.age_at_assertion::text as "age_at_assertion",
        GEN_UNKNOWN.age_at_event::text as "age_at_event",
        GEN_UNKNOWN.age_at_resolution::text as "age_at_resolution",
@@ -16,6 +17,26 @@
        {{ generate_global_id(prefix='',descriptor=[''], study_id='gregor_synthetic') }}::text as "has_access_policy",
        {{ generate_global_id(prefix='sa',descriptor=['participant.anvil_gregor_gss_u07_gru_participant_id'], study_id='gregor_synthetic') }}::text as "id",
        {{ generate_global_id(prefix='sb',descriptor=['phenotype.participant_id'], study_id='gregor_synthetic') }}::text as "Subject_id"
+=======
+        GEN_UNKNOWN.age_at_assertion::text as "age_at_assertion",
+        phenotype.onset_age_range::text as "age_at_event",
+        GEN_UNKNOWN.age_at_resolution::text as "age_at_resolution",
+        GEN_UNKNOWN.code::text as "code",
+        GEN_UNKNOWN.display::text as "display",
+        CASE phenotype.presence
+            WHEN 'Present' THEN 'SNOMED:373066001'
+            WHEN 'Absent' THEN 'SNOMED:373067005'
+            WHEN 'Unknown' THEN 'SNOMED:261665006'
+            ELSE phenotype.presence
+        END::text as "value_code",
+        phenotype.presence::text as "value_display",
+        GEN_UNKNOWN.value_number::text as "value_number",
+        GEN_UNKNOWN.value_units::text as "value_units",
+        GEN_UNKNOWN.value_units_display::text as "value_units_display",
+        GEN_UNKNOWN.has_access_policy::text as "has_access_policy",
+        GEN_UNKNOWN.id::text as "id",
+        phenotype.participant_id::text as "Subject_id"
+>>>>>>> de565fd (Started work on phenotype transforms)
         from {{ ref('gregor_synthetic_stg_participant') }} as participant
         join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype
 on participant.anvil_gregor_gss_u07_gru_participant_id = phenotype.participant_id 
