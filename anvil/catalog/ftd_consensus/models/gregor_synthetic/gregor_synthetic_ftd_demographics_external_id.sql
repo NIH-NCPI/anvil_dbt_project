6888cff7e32a02bc18449567a6a9d1c4a15efbdd
Yelena Cox <yelenacox@gmail.com>
2025-05-28 12:45:57 -0500

    Modified external id's in external id files

--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_demographics_external_id.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_demographics_external_id.sql
@@ -7,11 +7,17 @@
        GEN_UNKNOWN.external_id::text as "external_id"
 =======
         {{ generate_global_id(prefix='dm',descriptor=['participant.AnVIL_GREGoR_GSS_U07_GRU_participant_id'], study_id='gregor_synthetic') }}::text as "Demographics_id",
+<<<<<<< HEAD
     --    GEN_UNKNOWN.external_id::text as "external_id"
 >>>>>>> f1f1cfe (Modified family relationship file, external_id files)
         from {{ ref('gregor_synthetic_stg_participant') }} as participant
         join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype
 on participant.anvil_gregor_gss_u07_gru_participant_id = phenotype.participant_id 
+=======
+        participant.AnVIL_GREGoR_GSS_U07_GRU_participant_id::text as "external_id"
+        from {{ ref('gregor_synthetic_stg_participant') }} as participant
+        -- join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype using (ftd_key)
+>>>>>>> 9cff46c (Modified external id's in external id files)
     )
 
     select 
--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_familymember_external_id.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_familymember_external_id.sql
@@ -7,11 +7,15 @@
 =======
         {{ generate_global_id(prefix='fm',descriptor=['participant.AnVIL_GREGoR_GSS_U07_GRU_participant_id'], study_id='gregor_synthetic') }}::text as "FamilyMember_id",
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> b81083e (Using global id function in external_id files)
        GEN_UNKNOWN.external_id::text as "external_id"
 =======
     --    GEN_UNKNOWN.external_id::text as "external_id"
 >>>>>>> f1f1cfe (Modified family relationship file, external_id files)
+=======
+        participant.AnVIL_GREGoR_GSS_U07_GRU_participant_id::text as "external_id"
+>>>>>>> 9cff46c (Modified external id's in external id files)
         from {{ ref('gregor_synthetic_stg_participant') }} as participant
         join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype
 on participant.anvil_gregor_gss_u07_gru_participant_id = phenotype.participant_id 
