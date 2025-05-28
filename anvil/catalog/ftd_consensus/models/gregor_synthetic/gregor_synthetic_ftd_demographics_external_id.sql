9cff46cf784d49345c3cbe8a781e34cddb7fff35
Yelena Cox <yelenacox@gmail.com>
2025-05-28 12:45:57 -0500

    Modified external id's in external id files

--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_demographics_external_id.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_demographics_external_id.sql
@@ -3,9 +3,9 @@
     with source as (
         select 
         {{ generate_global_id(prefix='dm',descriptor=['participant.AnVIL_GREGoR_GSS_U07_GRU_participant_id'], study_id='gregor_synthetic') }}::text as "Demographics_id",
-    --    GEN_UNKNOWN.external_id::text as "external_id"
+        participant.AnVIL_GREGoR_GSS_U07_GRU_participant_id::text as "external_id"
         from {{ ref('gregor_synthetic_stg_participant') }} as participant
-        join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype using (ftd_key)
+        -- join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype using (ftd_key)
     )
 
     select 
--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_subjectassertion_external_id.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_subjectassertion_external_id.sql
@@ -3,7 +3,7 @@
     with source as (
         select 
         {{ generate_global_id(prefix='sa',descriptor=['phenotype.AnVIL_GREGoR_GSS_U07_GRU_phenotype_id'], study_id='gregor_synthetic') }}::text as "SubjectAssertion_id",
-        -- GEN_UNKNOWN.external_id::text as "external_id"
+        phenotype.AnVIL_GREGoR_GSS_U07_GRU_phenotype_id::text as "external_id"
         from {{ ref('gregor_synthetic_stg_participant') }} as participant
         join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype
         on participant.AnVIL_GREGoR_GSS_U07_GRU_participant_id = phenotype.participant_id 
