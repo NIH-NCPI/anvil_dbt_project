bc7db193f2627b507f3f776212324c82526b6786
Yelena Cox <yelenacox@gmail.com>
2025-05-28 15:16:15 -0500

    Modified query_duckdb to query all tables being used

--- scripts/query_duckdb.py
+++ scripts/query_duckdb.py
@@ -15,6 +15,36 @@ d = con.execute(
 ).fetchall()
 print(f"FTD Demographics\n {d}\n")
 
+de = con.execute(
+    "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_demographics_external_id LIMIT 10"
+).fetchall()
+print(f"FTD Demographics\n {de}\n")
+
+dr = con.execute(
+    "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_demographics_race LIMIT 10"
+).fetchall()
+print(f"FTD Demographics\n {dr}\n")
+
+fm = con.execute(
+    "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_familymember LIMIT 10"
+).fetchall()
+print(f"FTD Demographics\n {fm}\n")
+
+fme = con.execute(
+    "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_familymember_external_id LIMIT 10"
+).fetchall()
+print(f"FTD Demographics\n {fme}\n")
+
+fr = con.execute(
+    "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_familyrelationship LIMIT 10"
+).fetchall()
+print(f"FTD Demographics\n {fr}\n")
+
+fre = con.execute(
+    "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_familyrelationship_external_id LIMIT 10"
+).fetchall()
+print(f"FTD Demographics\n {fre}\n")
+
 # ap = con.execute(
 #     "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_accesspolicy LIMIT 10"
 # ).fetchall()
@@ -29,6 +59,16 @@ se = con.execute(
     "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_subject_external_id LIMIT 10"
 ).fetchall()
 print(f"FTD Subject External Id\n {se}\n")
+
+sa = con.execute(
+    "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_subjectassertion LIMIT 10"
+).fetchall()
+print(f"FTD Subject\n {sa}\n")
+
+sae = con.execute(
+    "aLECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_subjectassertion_external_id LIMIT 10"
+).fetchall()
+print(f"FTD Subject External Id\n {sae}\n")
 
 apapc = con.execute(
     "SELECT * FROM main_gregor_synthetic_data.gregor_synthetic_ftd_accesspolicy_access_policy_code LIMIT 10"
