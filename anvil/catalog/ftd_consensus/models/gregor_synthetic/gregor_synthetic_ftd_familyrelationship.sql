f26977ef04a58f80d97293f88886ece01334d75f
Yelena Cox <yelenacox@gmail.com>
2025-06-11 11:53:28 -0500

    Modified familyrelationship

--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_familyrelationship.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_familyrelationship.sql
@@ -38,8 +38,8 @@
         {{ generate_global_id(prefix='ap',descriptor=['participant.consent_code'], study_id='gregor_synthetic') }}::text as "has_access_policy",
         {{ generate_global_id(prefix='fr',descriptor=['participant.anvil_gregor_gss_u07_gru_participant_id'], study_id='gregor_synthetic') }}::text as "id",
         from {{ ref('gregor_synthetic_stg_participant') }} as participant
-        join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype
-on participant.anvil_gregor_gss_u07_gru_participant_id = phenotype.participant_id 
+--         join {{ ref('gregor_synthetic_stg_phenotype') }} as phenotype
+-- on participant.anvil_gregor_gss_u07_gru_participant_id = phenotype.participant_id 
     )
 
     select 
