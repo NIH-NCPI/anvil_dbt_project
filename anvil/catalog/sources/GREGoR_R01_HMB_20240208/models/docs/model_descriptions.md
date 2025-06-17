eee2f20c01f616d5fe9094b296f311f20ac2b619
Yelena Cox <yelenacox.gmail.com>
2025-06-17 20:53:56 +0000

    Participant and Phenotype harmonizations

---
+++ anvil/catalog/sources/GREGoR_R01_HMB_20240208/models/docs/model_descriptions.md
@@ -0,0 +1,9 @@
+### Participant Models
+
+{% docs GREGoR_R01_HMB_20240208_participant %}
+Model for GREGoR_R01_HMB_20240208_participant.
+{% enddocs %}
+
+{% docs GREGoR_R01_HMB_20240208_stg_participant %}
+Model for GREGoR_R01_HMB_20240208_stg_participant.
+{% enddocs %}
--- scripts/examples/ex_run_commands.sh
+++ scripts/examples/ex_run_commands.sh
@@ -4,29 +4,30 @@ dbt deps  || { echo "Error: dbt deps failed. Exiting..."; exit 1; }
 # 
 
 # gregor/duckdb examples
-dbt run --select  GREGoR_HMB_20240208_stg_participant
-dbt run --select  GREGoR_HMB_20240208_stg_phenotype
-dbt run --select  GREGoR_HMB_20240208_ftd_subject
-dbt run --select  GREGoR_HMB_20240208_ftd_subject_external_id
-dbt run --select  GREGoR_HMB_20240208_ftd_subjectassertion
-dbt run --select  GREGoR_HMB_20240208_ftd_subjectassertion_external_id
-dbt run --select  GREGoR_HMB_20240208_ftd_demographics
-dbt run --select  GREGoR_HMB_20240208_ftd_demographics_external_id
-dbt run --select  GREGoR_HMB_20240208_ftd_demographics_race
-dbt run --select  GREGoR_HMB_20240208_ftd_familyrelationship
-dbt run --select  GREGoR_HMB_20240208_ftd_familyrelationship_external_id
-dbt run --select  GREGoR_HMB_20240208_ftd_familymember
-dbt run --select  GREGoR_HMB_20240208_ftd_familymember_external_id
-dbt run --select  GREGoR_HMB_20240208_ftd_accesspolicy_access_policy_code
-dbt run --select  GREGoR_HMB_20240208_ftd_accesspolicy
-# dbt run --select  GREGoR_HMB_20240208_ftd_participant
-# dbt run --select  GREGoR_HMB_20240208_ftd_phenotype
-# dbt run --select tgt_participant --vars '{"source_table": "GREGoR_HMB_20240208_ftd_participant", "target_schema": "main"}'
-# dbt run --select tgt_phenotype --vars '{"source_table": "GREGoR_HMB_20240208_ftd_phenotype", "target_schema": "main"}'
+# dbt run --select  GREGoR_R01_HMB_20240208_stg_participant
+# dbt run --select  GREGoR_R01_HMB_20240208_stg_phenotype
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_subject
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_subject_external_id
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_subjectassertion
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_subjectassertion_external_id
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_demographics
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_demographics_external_id
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_demographics_race
+dbt run --select  GREGoR_R01_HMB_20240208_ftd_familyrelationship
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_familyrelationship_external_id
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_familymember
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_familymember_external_id
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_accesspolicy_access_policy_code
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_accesspolicy
+
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_participant
+# dbt run --select  GREGoR_R01_HMB_20240208_ftd_phenotype
+# dbt run --select tgt_participant --vars '{"source_table": "GREGoR_R01_HMB_20240208_ftd_participant", "target_schema": "main"}'
+# dbt run --select tgt_phenotype --vars '{"source_table": "GREGoR_R01_HMB_20240208_ftd_phenotype", "target_schema": "main"}'
 
 # Run a single target model and its dependencies.
-# dbt run --select tgt_participant --vars '{"source_table": "GREGoR_HMB_20240208_ftd_participant", "target_schema": "GREGoR_HMB_20240208_tgt_data"}'
-# dbt run --select tgt_condition --vars '{"source_table": "GREGoR_HMB_20240208_ftd_condition", "target_schema": "GREGoR_HMB_20240208_tgt_data"}'
-# dbt run --select +tgt_event --vars '{"source_table": "GREGoR_HMB_20240208_ftd_event", "target_schema": "GREGoR_HMB_20240208_tgt_data"}'
-# dbt run --select +tgt_measurement --vars '{"source_table": "GREGoR_HMB_20240208_ftd_measurement", "target_schema": "GREGoR_HMB_20240208_tgt_data"}'
-# dbt run --select tgt_study --vars '{"source_table": "GREGoR_HMB_20240208_ftd_study", "target_schema": "GREGoR_HMB_20240208_tgt_data"}'
+# dbt run --select tgt_participant --vars '{"source_table": "GREGoR_R01_HMB_20240208_ftd_participant", "target_schema": "GREGoR_R01_HMB_20240208_tgt_data"}'
+# dbt run --select tgt_condition --vars '{"source_table": "GREGoR_R01_HMB_20240208_ftd_condition", "target_schema": "GREGoR_R01_HMB_20240208_tgt_data"}'
+# dbt run --select +tgt_event --vars '{"source_table": "GREGoR_R01_HMB_20240208_ftd_event", "target_schema": "GREGoR_R01_HMB_20240208_tgt_data"}'
+# dbt run --select +tgt_measurement --vars '{"source_table": "GREGoR_R01_HMB_20240208_ftd_measurement", "target_schema": "GREGoR_R01_HMB_20240208_tgt_data"}'
+# dbt run --select tgt_study --vars '{"source_table": "GREGoR_R01_HMB_20240208_ftd_study", "target_schema": "GREGoR_R01_HMB_20240208_tgt_data"}'
