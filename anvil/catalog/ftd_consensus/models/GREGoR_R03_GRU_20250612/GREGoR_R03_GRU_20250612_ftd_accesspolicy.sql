e5d5bd4fe334e6f8116f2f7e3913d6f95d88acfc
Yelena Cox <yelenacox.gmail.com>
2025-07-09 20:56:29 +0000

    Adding dbt tests

--- anvil/catalog/ftd_consensus/models/GREGoR_R03_GRU_20250612/GREGoR_R03_GRU_20250612_ftd_accesspolicy.sql
+++ anvil/catalog/ftd_consensus/models/GREGoR_R03_GRU_20250612/GREGoR_R03_GRU_20250612_ftd_accesspolicy.sql
@@ -3,7 +3,18 @@
     with source as (
         select 
     --     GEN_UNKNOWN.disease_limitation::text as "disease_limitation",
-    --    GEN_UNKNOWN.description::text as "description",
+    CASE participant.consent_code
+        WHEN 'GRU' THEN 'General Research Use'
+        WHEN 'HMB' THEN 'Health/Medical/Biomedical'
+        WHEN 'DS' THEN 'Disease-Specific (Disease/Trait/Exposure)'
+        WHEN 'IRB' THEN 'IRB Approval Required'
+        WHEN 'PUB' THEN 'Publication Required'
+        WHEN 'COL' THEN 'Collaboration Required'
+        WHEN 'NPU' THEN 'Not-for-profit use only'
+        WHEN 'MDS' THEN 'Methods'
+        WHEN 'GSO' THEN 'Genetic Studies only'
+        WHEN 'GSR' THEN 'Genomic Summary Results'
+       END::text as "description",
     --    GEN_UNKNOWN.website::text as "website",
       {{ generate_global_id(prefix='ap',descriptor=['participant.consent_code'], study_id='GREGoR_R03_GRU_20250612') }}::text as "id"
         from {{ ref('GREGoR_R03_GRU_20250612_stg_participant') }} as participant
