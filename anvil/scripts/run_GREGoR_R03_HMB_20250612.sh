0d94e0633ee4fe6f0f3b9b7f3f47e376f49c5f4c
Yelena Cox <107718499+yelenacox@users.noreply.github.com>
2025-07-17 13:44:54 -0500

    Update run_GREGoR_R03_HMB_20250612.sh

--- anvil/scripts/run_GREGoR_R03_HMB_20250612.sh
+++ anvil/scripts/run_GREGoR_R03_HMB_20250612.sh
@@ -1,49 +1,43 @@
-#!/bin/bash
-set -e  # Exit on error
 dbt clean
-dbt deps || { echo "Error: dbt deps failed. Exiting..."; exit 1; }
-# Run Target tables
-dbt run --select +tgt_accesscontrolledrecord_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_accesscontrolledrecord_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_aliquot_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_aliquot_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_accesspolicy_data_access_type --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_accesspolicy_data_access_type", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_family_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_family_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_file --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_file", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_subjectassertion_source_data --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_subjectassertion_source_data", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_file_subject --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_file_subject", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_subject --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_subject", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_study_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_study_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_familymember_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_familymember_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_file_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_file_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_familymember --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_familymember", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_demographics_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_demographics_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_sample_storage_method --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_sample_storage_method", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_study --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_study", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_accesspolicy_access_policy_code --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_accesspolicy_access_policy_code", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_accesspolicy_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_accesspolicy_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_thing_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_thing_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_accesscontrolledrecord --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_accesscontrolledrecord", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_family --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_family", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_sourcedata_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_sourcedata_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_subject_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_subject_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_biospecimencollection_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_biospecimencollection_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_demographics --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_demographics", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_study_funding_source --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_study_funding_source", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_sample --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_sample", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_familyrelationship --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_familyrelationship", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_filemetadata --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_filemetadata", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_subjectassertion --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_subjectassertion", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_accesspolicy --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_accesspolicy", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_sample_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_sample_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_filemetadata_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_filemetadata_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_biospecimencollection --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_biospecimencollection", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_aliquot --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_aliquot", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_sourcedata --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_sourcedata", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_study_external_study_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_study_external_study_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_file_sample --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_file_sample", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_study_principal_investigator --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_study_principal_investigator", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_demographics_source_data --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_demographics_source_data", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_family_family_relationships --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_family_family_relationships", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_familyrelationship_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_familyrelationship_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_sample_processing --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_sample_processing", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_demographics_race --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_demographics_race", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
-dbt run --select +tgt_subjectassertion_external_id --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_subjectassertion_external_id", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
+dbt deps  || { echo "Error: dbt deps failed. Exiting..."; exit 1; }
+# dbt seed
+# 
+
+# gregor/duckdb examples
+dbt run --select  GREGoR_R03_HMB_20250612_stg_participant
+dbt run --select  GREGoR_R03_HMB_20250612_stg_phenotype
+dbt run --select  GREGoR_R03_HMB_20250612_stg_experiment
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_subject
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_subject_external_id
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_subjectassertion
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_subjectassertion_external_id
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_demographics
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_demographics_external_id
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_demographics_race
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_familyrelationship
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_familyrelationship_external_id
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_familymember
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_familymember_external_id
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_accesspolicy_access_policy_code
+dbt run --select  GREGoR_R03_HMB_20250612_ftd_accesspolicy
+dbt test --select GREGoR_R03_HMB_20250612_ftd_subject
+dbt test --select GREGoR_R03_HMB_20250612_ftd_subjectassertion
+dbt test --select GREGoR_R03_HMB_20250612_ftd_demographics
+dbt test --select GREGoR_R03_HMB_20250612_ftd_demographics_race
+dbt test --select GREGoR_R03_HMB_20250612_ftd_familyrelationship
+dbt test --select GREGoR_R03_HMB_20250612_ftd_familymember
+dbt test --select GREGoR_R03_HMB_20250612_ftd_accesspolicy_access_policy_code
+dbt test --select GREGoR_R03_HMB_20250612_ftd_accesspolicy
+
+
+# dbt run --select  GREGoR_R03_HMB_20250612_ftd_participant
+# dbt run --select  GREGoR_R03_HMB_20250612_ftd_phenotype
+# dbt run --select tgt_participant --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_participant", "target_schema": "main"}'
+# dbt run --select tgt_phenotype --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_phenotype", "target_schema": "main"}'
+
+# Run a single target model and its dependencies.
+# dbt run --select tgt_participant --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_participant", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
+# dbt run --select tgt_condition --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_condition", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
+# dbt run --select +tgt_event --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_event", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
+# dbt run --select +tgt_measurement --vars '{"source_table": "GREGoR_R03_HMB_20250612_ftd_measurement", "target_schema": "GREGoR_R03_HMB_20250612_tgt_data"}'
+# dbt run --select tgt_study --vars '{"source_table": "GREGoR_R03_HMB_202HMB12_ftd_study", "target_schema": "GREGoR_R03_HMB_20HHMB12_tgt_dataHMB
