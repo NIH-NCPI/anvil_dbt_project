5b93b3cb40086fe421b7c04709549f57ae46dcec
Yelena Cox <107718499+yelenacox@users.noreply.github.com>
2025-07-17 13:19:48 -0500

    Update ex_run_commands.sh

--- scripts/examples/ex_run_commands.sh
+++ scripts/examples/ex_run_commands.sh
@@ -4,64 +4,19 @@ dbt deps  || { echo "Error: dbt deps failed. Exiting..."; exit 1; }
 # 
 
 # gregor/duckdb examples
-dbt run --select  GREGoR_R03_GRU_20250612_stg_participant
-dbt run --select  GREGoR_R03_GRU_20250612_stg_phenotype
-dbt run --select  GREGoR_R03_GRU_20250612_stg_experiment
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_subject
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_subject_external_id
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_subjectassertion
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_subjectassertion_external_id
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_demographics
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_demographics_external_id
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_demographics_race
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_familyrelationship
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_familyrelationship_external_id
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_familymember
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_familymember_external_id
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_accesspolicy_access_policy_code
-dbt run --select  GREGoR_R03_GRU_20250612_ftd_accesspolicy
-dbt test --select GREGoR_R03_GRU_20250612_ftd_subject
-dbt test --select GREGoR_R03_GRU_20250612_ftd_subjectassertion
-dbt test --select GREGoR_R03_GRU_20250612_ftd_demographics
-dbt test --select GREGoR_R03_GRU_20250612_ftd_demographics_race
-dbt test --select GREGoR_R03_GRU_20250612_ftd_familyrelationship
-dbt test --select GREGoR_R03_GRU_20250612_ftd_familymember
-dbt test --select GREGoR_R03_GRU_20250612_ftd_accesspolicy_access_policy_code
-dbt test --select GREGoR_R03_GRU_20250612_ftd_accesspolicy
-dbt run --select  GREGoR_R03_HMB_20250612_stg_participant
-dbt run --select  GREGoR_R03_HMB_20250612_stg_phenotype
-dbt run --select  GREGoR_R03_HMB_20250612_stg_experiment
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_subject
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_subject_external_id
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_subjectassertion
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_subjectassertion_external_id
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_demographics
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_demographics_external_id
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_demographics_race
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_familyrelationship
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_familyrelationship_external_id
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_familymember
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_familymember_external_id
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_accesspolicy_access_policy_code
-dbt run --select  GREGoR_R03_HMB_20250612_ftd_accesspolicy
-dbt test --select GREGoR_R03_HMB_20250612_ftd_subject
-dbt test --select GREGoR_R03_HMB_20250612_ftd_subjectassertion
-dbt test --select GREGoR_R03_HMB_20250612_ftd_demographics
-dbt test --select GREGoR_R03_HMB_20250612_ftd_demographics_race
-dbt test --select GREGoR_R03_HMB_20250612_ftd_familyrelationship
-dbt test --select GREGoR_R03_HMB_20250612_ftd_familymember
-dbt test --select GREGoR_R03_HMB_20250612_ftd_accesspolicy_access_policy_code
-dbt test --select GREGoR_R03_HMB_20250612_ftd_accesspolicy
+dbt run --select  gregor_synthetic_stg_participant
+dbt run --select  gregor_synthetic_stg_phenotype
 
+# dbt run --select  gregor_synthetic_ftd_accesspolicy
+# dbt run --select  gregor_synthetic_ftd_demographics
+dbt run --select  gregor_synthetic_ftd_demographics
 
-# dbt run --select  GREGoR_R03_GRU_20250612_ftd_participant
-# dbt run --select  GREGoR_R03_GRU_20250612_ftd_phenotype
-# dbt run --select tgt_participant --vars '{"source_table": "GREGoR_R03_GRU_20250612_ftd_participant", "target_schema": "main"}'
-# dbt run --select tgt_phenotype --vars '{"source_table": "GREGoR_R03_GRU_20250612_ftd_phenotype", "target_schema": "main"}'
+# dbt run --select tgt_participant --vars '{"source_table": "gregor_synthetic_ftd_participant", "target_schema": "main"}'
+# dbt run --select tgt_phenotype --vars '{"source_table": "gregor_synthetic_ftd_phenotype", "target_schema": "main"}'
 
 # Run a single target model and its dependencies.
-# dbt run --select tgt_participant --vars '{"source_table": "GREGoR_R03_GRU_20250612_ftd_participant", "target_schema": "GREGoR_R03_GRU_20250612_tgt_data"}'
-# dbt run --select tgt_condition --vars '{"source_table": "GREGoR_R03_GRU_20250612_ftd_condition", "target_schema": "GREGoR_R03_GRU_20250612_tgt_data"}'
-# dbt run --select +tgt_event --vars '{"source_table": "GREGoR_R03_GRU_20250612_ftd_event", "target_schema": "GREGoR_R03_GRU_20250612_tgt_data"}'
-# dbt run --select +tgt_measurement --vars '{"source_table": "GREGoR_R03_GRU_20250612_ftd_measurement", "target_schema": "GREGoR_R03_GRU_20250612_tgt_data"}'
-# dbt run --select tgt_study --vars '{"source_table": "GREGoR_R03_GRU_20250612_ftd_study", "target_schema": "GREGoR_R03_GRU_20250612_tgt_data"}'
+# dbt run --select tgt_participant --vars '{"source_table": "gregor_synthetic_ftd_participant", "target_schema": "gregor_synthetic_tgt_data"}'
+# dbt run --select tgt_condition --vars '{"source_table": "gregor_synthetic_ftd_condition", "target_schema": "gregor_synthetic_tgt_data"}'
+# dbt run --select +tgt_event --vars '{"source_table": "gregor_synthetic_ftd_event", "target_schema": "gregor_synthetic_tgt_data"}'
+# dbt run --select +tgt_measurement --vars '{"source_table": "gregor_synthetic_ftd_measurement", "target_schema": "gregor_synthetic_tgt_data"}'
+# dbt run --select tgt_study --vars '{"source_table": "gregor_synthetic_ftd_study", "target_schema": "gregor_synthetic_tgt_data"}'
