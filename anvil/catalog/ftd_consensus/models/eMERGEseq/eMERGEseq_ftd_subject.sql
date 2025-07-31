a8e6cf108e39e9f5a8eaebcaacb12d4e9b45ddcf
Yelena Cox <yelenacox@gmail.com>
2025-07-31 21:07:46 +0000

    Modifying subject table

--- anvil/catalog/ftd_consensus/models/eMERGEseq/eMERGEseq_ftd_subject.sql
+++ anvil/catalog/ftd_consensus/models/eMERGEseq/eMERGEseq_ftd_subject.sql
@@ -1,20 +1,18 @@
 {{ config(materialized='table', schema='eMERGEseq_data') }}
 
     with source as (
-        select 
+        select DISTINCT
          CASE 
             WHEN sample.subject_id IS NOT NULL THEN 'participant'
             ELSE 'non_participant'
         END::text as subject_type,
         --        GEN_UNKNOWN.organism_type::text as "organism_type",
        {{ generate_global_id(prefix='ap',descriptor=['subjectconsent.consent'], study_id='phs001616') }}::text as "has_access_policy",
-       {{ generate_global_id(prefix='sb',descriptor=['demographics.subject_id'], study_id='phs001616') }}::text as "id"
+       {{ generate_global_id(prefix='sb',descriptor=['subjectconsent.subject_id'], study_id='phs001616') }}::text as "id"
 --        { { generate_global_id(prefix='',descriptor=[''], study_id='eMERGEseq') }}::text as "has_demographics_id"
-        from {{ ref('eMERGEseq_stg_demographics') }} as demographics
+        from {{ ref('eMERGEseq_stg_subjectconsent') }} as subjectconsent
         left join {{ ref('eMERGEseq_stg_samplesubjectmapping') }} as sample
-        on demographics.subject_id = sample.subject_id
-        left join {{ ref('eMERGEseq_stg_subjectconsent') }} as subjectconsent
-        on demographics.subject_id = subjectconsent.subject_id
+        on subjectconsent.subject_id = sample.subject_id
     )
 
     select 
