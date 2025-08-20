1e21686b2392b47e7de18a58425bdbbde54d64ca
yelenacox <yelenacox@gmail.com>
2025-08-20 21:08:59 +0000

    Starting on accespolicy

--- anvil/catalog/ftd_consensus/models/GWAS/GWAS_ftd_accesspolicy.sql
+++ anvil/catalog/ftd_consensus/models/GWAS/GWAS_ftd_accesspolicy.sql
@@ -1,17 +1,37 @@
 {{ config(materialized='table', schema='GWAS_data') }}
 
-select 
-GEN_UNKNOWN.disease_limitation::text as "disease_limitation",
-  GEN_UNKNOWN.description::text as "description",
-  GEN_UNKNOWN.website::text as "website",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='GWAS') }}::text as "id"
-from {{ ref('GWAS_stg_bmi') }} as bmi
-join {{ ref('GWAS_stg_casecontrol') }} as casecontrol
-on   join {{ ref('GWAS_stg_demographics') }} as demographics
-on subjectconsent.subject_id = demographics.subject_id  join {{ ref('GWAS_stg_pedigree') }} as pedigree
-on   join {{ ref('GWAS_stg_phecode') }} as phecode
-on   join {{ ref('GWAS_stg_sampleattributes') }} as sampleattributes
-on samplesubjectmapping.sample_id = sampleattributes.sample_id  join {{ ref('GWAS_stg_samplesubjectmapping') }} as samplesubjectmapping
-on sampleattributes.sample_id = samplesubjectmapping.sample_id  join {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
-on demographics.subject_id = subjectconsent.subject_id 
+{%- set consent_groups = [
+    {'description': 'General Research Use', 'consents': ['1', '2', '3', '9']},
+    {'description': 'IRB Approval Required', 'consents': ['2', '3', '9']},
+    {'description': 'Collaboration Required', 'consents': ['3']},
+    {'description': 'Not-for-profit use only', 'consents': ['3']},
+    {'description': 'Publication Required', 'consents': ['2', '8', '9']},
+    {'description': 'Health/Medical/Biomedical', 'consents': ['4', '5', '8', '10']},
+    {'description': 'Genetic Studies only', 'consents': ['2', '8', '10']},
+    {'description': 'Methods', 'consents': ['5']},
+    {'description': 'Disease', 'consents': ['6', '7']}
+] -%}
 
+with source as (
+    {%- for group in consent_groups -%}
+    (
+    select DISTINCT
+        NULL as "disease_limitation",
+        '{{ group.description }}' as "description",
+        NULL as "website",
+        {{ generate_global_id(prefix='ap', descriptor=['subjectconsent.consent'], study_id='phs001616') }}::text as "id"
+    from {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
+      where subjectconsent.consent in (
+        {%- for c in group.consents -%}
+            '{{ c }}'{% if not loop.last %}, 
+          {% endif %}
+        {%- endfor -%}
+          )
+        )
+    {% if not loop.last %}union all{% endif %}
+    {%- endfor -%}
+
+)
+select * from source
+
+    
