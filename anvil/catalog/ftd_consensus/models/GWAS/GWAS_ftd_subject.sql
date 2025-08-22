8c338e336004d1ad96a0232dc82642ca5b6bc7cc
yelenacox <yelenacox@gmail.com>
2025-08-22 15:19:47 +0000

    Subject and subject external id

--- anvil/catalog/ftd_consensus/models/GWAS/GWAS_ftd_subject.sql
+++ anvil/catalog/ftd_consensus/models/GWAS/GWAS_ftd_subject.sql
@@ -1,18 +1,15 @@
 {{ config(materialized='table', schema='GWAS_data') }}
 
 select 
-GEN_UNKNOWN.subject_type::text as "subject_type",
-  GEN_UNKNOWN.organism_type::text as "organism_type",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='GWAS') }}::text as "has_access_policy",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='GWAS') }}::text as "id",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='GWAS') }}::text as "has_demographics_id"
-from {{ ref('GWAS_stg_bmi') }} as bmi
-join {{ ref('GWAS_stg_casecontrol') }} as casecontrol
-on   join {{ ref('GWAS_stg_demographics') }} as demographics
-on subjectconsent.subject_id = demographics.subject_id  join {{ ref('GWAS_stg_pedigree') }} as pedigree
-on   join {{ ref('GWAS_stg_phecode') }} as phecode
-on   join {{ ref('GWAS_stg_sampleattributes') }} as sampleattributes
-on samplesubjectmapping.sample_id = sampleattributes.sample_id  join {{ ref('GWAS_stg_samplesubjectmapping') }} as samplesubjectmapping
-on sampleattributes.sample_id = samplesubjectmapping.sample_id  join {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
-on demographics.subject_id = subjectconsent.subject_id 
+  CASE 
+      WHEN subjectconsent.consent != 0 THEN 'participant'
+      ELSE 'non_participant'
+  END::text as subject_type,  
+  NULL as "organism_type",
+    {{ generate_global_id(prefix='ap',descriptor=['subjectconsent.consent'], study_id='phs001584') }}::text as "has_access_policy",
+    {{ generate_global_id(prefix='sb',descriptor=['subjectconsent.subject_id'], study_id='phs001584') }}::text as "id",
+    {{ generate_global_id(prefix='dm',descriptor=['demographics.subject_id'], study_id='phs001584') }}::text as "has_demographics_id"
+from {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
+join {{ ref('GWAS_stg_demographics') }} as demographics
+using (subject_id)
 
