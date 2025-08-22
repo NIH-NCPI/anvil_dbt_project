7e6aa5fa002c8e82dcd2a0456fdfac35b024710e
yelenacox <yelenacox@gmail.com>
2025-08-22 21:00:44 +0000

    Starting subjectassertion model

--- anvil/catalog/ftd_consensus/models/GWAS/GWAS_ftd_subjectassertion.sql
+++ anvil/catalog/ftd_consensus/models/GWAS/GWAS_ftd_subjectassertion.sql
@@ -1,27 +1,49 @@
 {{ config(materialized='table', schema='GWAS_data') }}
 
-select 
-GEN_UNKNOWN.assertion_type::text as "assertion_type",
-  GEN_UNKNOWN.age_at_assertion::text as "age_at_assertion",
-  GEN_UNKNOWN.age_at_event::text as "age_at_event",
-  GEN_UNKNOWN.age_at_resolution::text as "age_at_resolution",
-  GEN_UNKNOWN.code::text as "code",
-  GEN_UNKNOWN.display::text as "display",
-  GEN_UNKNOWN.value_code::text as "value_code",
-  GEN_UNKNOWN.value_display::text as "value_display",
-  GEN_UNKNOWN.value_number::text as "value_number",
-  GEN_UNKNOWN.value_units::text as "value_units",
-  GEN_UNKNOWN.value_units_display::text as "value_units_display",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='GWAS') }}::text as "has_access_policy",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='GWAS') }}::text as "id",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='GWAS') }}::text as "subject_id"
-from {{ ref('GWAS_stg_bmi') }} as bmi
-join {{ ref('GWAS_stg_casecontrol') }} as casecontrol
-on   join {{ ref('GWAS_stg_demographics') }} as demographics
-on subjectconsent.subject_id = demographics.subject_id  join {{ ref('GWAS_stg_pedigree') }} as pedigree
-on   join {{ ref('GWAS_stg_phecode') }} as phecode
-on   join {{ ref('GWAS_stg_sampleattributes') }} as sampleattributes
-on samplesubjectmapping.sample_id = sampleattributes.sample_id  join {{ ref('GWAS_stg_samplesubjectmapping') }} as samplesubjectmapping
-on sampleattributes.sample_id = samplesubjectmapping.sample_id  join {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
-on demographics.subject_id = subjectconsent.subject_id 
+with phecode_cte as (
+    select 
+'ehr_billing_code' as "assertion_type",
+  NULL as "age_at_assertion",
+  phecode.age_at_observation as "age_at_event",
+  NULL as "age_at_resolution",
+  phecode.phecode::text as "code",
+  NULL as "display",
+  NULL as "value_code",
+  NULL as "value_display",
+  NULL as "value_number",
+  NULL as "value_units",
+  NULL as "value_units_display",
+    {{ generate_global_id(prefix='ap',descriptor=['subjectconsent.consent'], study_id='phs001584') }}::text as "has_access_policy",
+    {{ generate_global_id(prefix='sa',descriptor=['phecode.subject_id', 'phecode.phecode'], study_id='phs001584') }}::text as "id",
+    {{ generate_global_id(prefix='sb',descriptor=['phecode.subject_id'], study_id='phs001584') }}::text as "subject_id"
+from {{ ref('GWAS_stg_phecode') }} as phecode
+left join {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
+using(subject_id) 
+  from {{ ref('GWAS_stg_phecode') }} as phecode
+ ) 
+ 
+ UNION ALL
+ 
+ bmi_cte as (
+ select
+  'measurement' as "assertion_type",
+  NULL as "age_at_assertion",
+  phecode.age_at_observation as "age_at_event",
+  NULL as "age_at_resolution",
+  phecode.phecode::text as "code",
+  NULL as "display",
+  NULL as "value_code",
+  NULL as "value_display",
+  NULL as "value_number",
+  NULL as "value_units",
+  NULL as "value_units_display",
+  {{ generate_global_id(prefix='ap',descriptor=['subjectconsent.consent'], study_id='phs001584') }}::text as "has_access_policy",
+  {{ generate_global_id(prefix='',descriptor=[''], study_id='phs001584') }}::text as "id",
+  {{ generate_global_id(prefix='',descriptor=[''], study_id='phs001584') }}::text as "subject_id"
+from {{ ref('GWAS_stg_phecode') }} as phecode
+left join {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
+using(subject_id) 
+  from {{ ref('GWAS_stg_bmi') }} as bmi
+ )
+    
 
