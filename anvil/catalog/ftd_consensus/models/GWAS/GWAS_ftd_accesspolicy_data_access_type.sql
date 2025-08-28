9749eab2181c23be200b3f717f7b77fbf4e6921f
yelenacox <yelenacox@gmail.com>
2025-08-28 20:06:17 +0000

    GWAS harmonizations

--- anvil/catalog/ftd_consensus/models/GWAS/GWAS_ftd_accesspolicy_data_access_type.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table', schema='GWAS_data') }}
-
-select 
-  {{ generate_global_id(prefix='',descriptor=[''], study_id='GWAS') }}::text as "accesspolicy_id",
-  GEN_UNKNOWN.data_access_type::text as "data_access_type"
-from {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
-
--- anvil/catalog/sources/GWAS/models/pedigree/GWAS_stg_pedigree.sql
+++ anvil/catalog/sources/GWAS/models/pedigree/GWAS_stg_pedigree.sql
@@ -4,16 +4,16 @@ with source as (
     select 
       "family_id"::text as "family_id",
        "subject_id"::text as "subject_id",
-        CASE WHEN mother = 'NA' THEN NULL
+      CASE WHEN mother = 'NA' THEN NULL
             ELSE mother
-        END::text as "mother",
+        END as "mother",
         CASE WHEN father = 'NA' THEN NULL
             ELSE father
-        END::text as "father",
-       "sex"::text as "sex",
-       CASE WHEN mz_twin_id = 'NA' THEN NULL
+        END as "father",
+        "sex"::text as "sex",
+        CASE WHEN mz_twin_id = 'NA' THEN NULL
             ELSE mz_twin_id
-       END::text as "mz_twin_id",
+        END as "mz_twin_id"
     from {{ source('GWAS','GWAS_Pedigree_DS_20200512') }}
 )
 
