30c521ece33115ad4ecb79bd66bddf326fdaff29
yelenacox <yelenacox@gmail.com>
2025-09-09 18:59:22 +0000

    Modifying unharmonized files

--- anvil/catalog/ftd_consensus/models/GWAS/unharmonized/GWAS_ftd_file.sql
+++ anvil/catalog/ftd_consensus/models/GWAS/unharmonized/GWAS_ftd_file.sql
@@ -9,12 +9,3 @@ null:text as "drs_uri",
 null::text as "file_metadata",
 null::text as "has_access_policy",
 null::text as "id"
-from {{ ref('GWAS_stg_bmi') }} as bmi
-join {{ ref('GWAS_stg_demographics') }} as demographics
-on subjectconsent.subject_id = demographics.subject_id  join {{ ref('GWAS_stg_pedigree') }} as pedigree
-on   join {{ ref('GWAS_stg_phecode') }} as phecode
-on   join {{ ref('GWAS_stg_sampleattributes') }} as sampleattributes
-on samplesubjectmapping.sample_id = sampleattributes.sample_id  join {{ ref('GWAS_stg_samplesubjectmapping') }} as samplesubjectmapping
-on sampleattributes.sample_id = samplesubjectmapping.sample_id  join {{ ref('GWAS_stg_subjectconsent') }} as subjectconsent
-on demographics.subject_id = subjectconsent.subject_id 
-
