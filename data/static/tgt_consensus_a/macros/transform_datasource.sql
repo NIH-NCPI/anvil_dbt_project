d661b8a704d3ec762924766932a971d0dab3c086
brendagutman <77469967+brendagutman@users.noreply.github.com>
2025-09-09 21:48:15 -0500

    Update transform_datasource.sql

--- data/static/tgt_consensus_a/macros/transform_datasource.sql
+++ data/static/tgt_consensus_a/macros/transform_datasource.sql
@@ -4,11 +4,7 @@ select distinct
   snapshot_id::text as "snapshot_id",
   google_data_project::text as "google_data_project",
   snapshot_dataset::text as "snapshot_dataset",
-<<<<<<< HEAD
   table_id::text as "table",
-=======
-  table_id::text as "table_id",
->>>>>>> edbbccf2c2325febf23c942b8d5333cdedf6a335
   parameterized_query::text as "parameterized_query",
   id::text as "id"
 from {{ ref(source_table) }}
