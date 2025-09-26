fba984e3f0d3e6451276c1eee07cccd1f0c68737
yelenacox <yelenacox@gmail.com>
2025-09-26 18:06:28 +0000

    Added consent to ids after merging main

--- anvil/catalog/ftd_consensus/models/PGRNseq/PGRNseq_ftd_demographics_external_id.sql
+++ anvil/catalog/ftd_consensus/models/PGRNseq/PGRNseq_ftd_demographics_external_id.sql
@@ -1,6 +1,8 @@
 {{ config(materialized='table', schema='PGRNseq_data') }}
 
 select 
-  {{ generate_global_id(prefix='dm',descriptor=['demographics.subject_id', 'subjectconsent.consent'], study_id='phs000906') }}::text as "demographics_id",
+  {{ generate_global_id(prefix='dm',descriptor=['demographics.subject_id', 'sc.consent'], study_id='phs000906') }}::text as "demographics_id",
   demographics.subject_id::text as "external_id"
 from {{ ref('PGRNseq_stg_demographics') }} as demographics
+left join {{ ref('PGRNseq_stg_subjectconsent') }} as sc
+using(subject_id)
