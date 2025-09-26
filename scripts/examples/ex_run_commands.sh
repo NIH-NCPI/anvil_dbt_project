48013a633ba9d26e8617df4b3a9a94f954b39270
Yelena Cox <107718499+yelenacox@users.noreply.github.com>
2025-09-26 14:16:44 -0500

    Update ex_run_commands.sh

--- scripts/examples/ex_run_commands.sh
+++ scripts/examples/ex_run_commands.sh
@@ -1,28 +1,15 @@
 dbt clean
 dbt deps  || { echo "Error: dbt deps failed. Exiting..."; exit 1; }
-dbt seed
+# dbt seed
 # 
 
-dbt run --select  GWAS_stg_subjectconsent
-dbt run --select  GWAS_stg_bmi
-dbt run --select  GWAS_stg_phecode
-# dbt run --select  PGRNseq_stg_demographics
-# dbt run --select  PGRNseq_stg_sampleattributions
-# dbt run --select  PGRNseq_stg_samplesubjectmapping
-# dbt run --select  PGRNseq_stg_ecg
-# dbt run --select  PGRNseq_stg_labs
+# gregor/duckdb examples
+dbt run --select  gregor_synthetic_stg_participant
+dbt run --select  gregor_synthetic_stg_phenotype
 
-# dbt build --select  PGRNseq_ftd_accesspolicy
-# dbt build --select  PGRNseq_ftd_accesspolicy_access_policy_code
-# dbt build --select  PGRNseq_ftd_demographics
-# dbt run --select  PGRNseq_ftd_demographics_external_id
-# dbt build --select  PGRNseq_ftd_demographics_race
-# dbt build --select  PGRNseq_ftd_sample
-# dbt run --select  PGRNseq_ftd_sample_external_id
-# dbt build --select  PGRNseq_ftd_subject
-# dbt run --select  PGRNseq_ftd_subject_external_id
-# dbt build --select  PGRNseq_ftd_subjectassertion
-dbt build --select GWAS_ftd_subjectassertion
+# dbt run --select  gregor_synthetic_ftd_accesspolicy
+# dbt run --select  gregor_synthetic_ftd_demographics
+dbt run --select  gregor_synthetic_ftd_demographics
 
 # dbt run --select tgt_participant --vars '{"source_table": "gregor_synthetic_ftd_participant", "target_schema": "main"}'
 # dbt run --select tgt_phenotype --vars '{"source_table": "gregor_synthetic_ftd_phenotype", "target_schema": "main"}'
