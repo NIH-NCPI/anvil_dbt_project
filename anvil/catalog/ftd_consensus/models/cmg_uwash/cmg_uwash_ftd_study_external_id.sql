87106b25bbae77561fe26d571f4adba5dbea33ad
yelenacox <yelenacox@gmail.com>
2025-10-29 21:10:28 +0000

    Began work on subjectassertion

--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_study_external_id.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_study_external_id.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table', schema='cmg_uwash_data') }}
 
-select 
+select distinct
   {{ generate_global_id(prefix='st',descriptor=['dataset_id'], study_id='phs000693') }}::text as "study_id",
   registered_identifier::text as "external_id"
 from {{ ref('cmg_uwash_stg_anvil_dataset') }} as anvil_dataset
--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion.sql
@@ -1,25 +1,43 @@
 {{ config(materialized='table', schema='cmg_uwash_data') }}
 
+with c_or_d as (
+    select 
+    subject_id,
+  NULL::text as "age_at_assertion",
+  NULL::text as "age_at_event",
+  NULL::text as "age_at_resolution",
+  NULL::text as "code",
+  NULL::text as "display",
+  NULL::text as "value_code",
+  NULL::text as "value_display",
+  NULL::text as "value_number",
+  NULL::text as "value_units",
+  NULL::text as "value_units_display",
+      UNNEST(SPLIT(disease_id, '|')) as d_code,
+      UNNEST(SPLIT(hpo_present, '|')) as c_code,
+    from {{ ref('cmg_uwash_stg_subject') }}
+ )
+ 
 select 
-GEN_UNKNOWN.assertion_type::text as "assertion_type",
-  GEN_UNKNOWN.age_at_assertion::text as "age_at_assertion",
-  GEN_UNKNOWN.age_at_event::text as "age_at_event",
-  GEN_UNKNOWN.age_at_resolution::text as "age_at_resolution",
-  GEN_UNKNOWN.code::text as "code",
-  GEN_UNKNOWN.display::text as "display",
-  GEN_UNKNOWN.value_code::text as "value_code",
-  GEN_UNKNOWN.value_display::text as "value_display",
-  GEN_UNKNOWN.value_number::text as "value_number",
-  GEN_UNKNOWN.value_units::text as "value_units",
-  GEN_UNKNOWN.value_units_display::text as "value_units_display",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='cmg_uwash') }}::text as "has_access_policy",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='cmg_uwash') }}::text as "id",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='cmg_uwash') }}::text as "subject_id"
-from {{ ref('cmg_uwash_stg_sample') }} as sample
-join {{ ref('cmg_uwash_stg_subject') }} as subject
-on sample.subject_id = subject.subject_id  join {{ ref('cmg_uwash_stg_anvil_dataset') }} as anvil_dataset
-on   join {{ ref('cmg_uwash_stg_sequencing') }} as sequencing
-on   join {{ ref('cmg_uwash_stg_family') }} as family
-on   join {{ ref('cmg_uwash_stg_file_inventory') }} as file_inventory
-on  
-
+  CASE 
+    WHEN d_code IS NOT NULL THEN 'disease'
+    WHEN c_code IS NOT NULL THEN 'phenotypic_feature'
+    ELSE NULL
+  END::text as "assertion_type",
+  age_at_assertion,
+  age_at_event,
+  age_at_resolution,
+  coalesce(d_code,c_code,null)::text as "code",
+  display,
+  value_code,
+  value_display,
+  value_number,
+  value_units,
+  value_units_display,
+    {{ generate_global_id(prefix='ap',descriptor=['consent_id'], study_id='phs000693') }}::text as "has_access_policy",
+    {{ generate_global_id(prefix='sa',descriptor=['subject_id', 'code'], study_id='phs000693') }}::text as "id",
+    subject_id
+--     { { generate_global_id(prefix='sb',descriptor=['subject_id', 'consent_id'], study_id='phs000693') }}::text as "subject_id"
+from {{ ref('cmg_uwash_stg_subject') }} as s
+left join c_or_d as cd
+using(subject_id)
