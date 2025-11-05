b36307ec7fb453eb4953b26ddc7f8defd9f155a7
yelenacox <yelenacox@gmail.com>
2025-11-05 22:02:07 +0000

    Annotations for subjectassertion

--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion.sql
@@ -1,34 +1,32 @@
 {{ config(materialized='table', schema='cmg_uwash_data') }}
 
-with c_or_d as (
+with unioned_codes as (
     select 
     subject_id,
-  NULL::text as "age_at_assertion",
-  NULL::text as "age_at_event",
-  NULL::text as "age_at_resolution",
-  NULL::text as "code",
-  NULL::text as "display",
-  NULL::text as "value_code",
-  affected_status::text as "value_display",
-  NULL::text as "value_number",
-  NULL::text as "value_units",
-  NULL::text as "value_units_display",
-      UNNEST(SPLIT(disease_id, '|')) as d_code,
-      UNNEST(SPLIT(hpo_present, '|')) as c_code,
-    from {{ ref('cmg_uwash_stg_subject') }}
- )
+    affected_status::text as "value_display",
+    UNNEST(SPLIT(replace(disease_id, ';',''), '|')) as code,
+  from {{ ref('cmg_uwash_stg_subject') }}
+ 
+ union all
+ 
+ select 
+    subject_id,
+    affected_status::text as "value_display",
+    UNNEST(SPLIT(replace(hpo_present, ';',''), '|')) as code,
+  from {{ ref('cmg_uwash_stg_subject') }}
+     )
  
 select 
   CASE 
-    WHEN d_code IS NOT NULL THEN 'disease'
-    WHEN c_code IS NOT NULL THEN 'phenotypic_feature'
+    WHEN code ilike '%OMIM%' THEN 'disease'
+    WHEN code ilike '%HP%' THEN 'phenotypic_feature'
     ELSE NULL
   END::text as "assertion_type",
-  age_at_assertion,
-  age_at_event,
-  age_at_resolution,
-  coalesce(d_code,c_code,null)::text as "code",
-  display,
+  NULL as age_at_assertion,
+  NULL as age_at_event,
+  NULL as age_at_resolution,
+  code,
+  cua.display as display,
   CASE 
         WHEN LOWER(affected_status) = 'affected' THEN 'LA9633-4'
         WHEN LOWER(affected_status) = 'unaffected' THEN 'LA9634-2'
@@ -37,16 +35,23 @@ select
         WHEN affected_status is null THEN 'LA4489-6'
         ELSE CONCAT('FTD_FLAG: unhandled value_code: ', affected_status)
   END::text as "value_code",
-  value_display,
-  value_number,
-  value_units,
-  value_units_display,
+  CASE 
+        WHEN LOWER(affected_status) = 'affected' THEN 'Present'
+        WHEN LOWER(affected_status) = 'unaffected' THEN 'Absent'
+        WHEN LOWER(affected_status) = 'unknown' THEN 'Unknown'
+        WHEN LOWER(affected_status) = 'possibly affected' THEN 'Possible'
+        WHEN affected_status is null THEN 'Unknown'
+        ELSE CONCAT('FTD_FLAG: unhandled value_code: ', affected_status)
+  END::text as "value_display",
+  NULL as value_number,
+  NULL as value_units,
+  NULL as value_units_display,
     {{ generate_global_id(prefix='ap',descriptor=['consent_id'], study_id='phs000693') }}::text as "has_access_policy",
     {{ generate_global_id(prefix='sa',descriptor=['subject_id', 'code'], study_id='phs000693') }}::text as "id",
     subject_id
 --     { { generate_global_id(prefix='sb',descriptor=['subject_id', 'consent_id'], study_id='phs000693') }}::text as "subject_id"
 from {{ ref('cmg_uwash_stg_subject') }} as s
-left join c_or_d as cd
+left join unioned_codes as uc
 using(subject_id)
-left join {{ ref('cmg_uwash_condition_mappings') }}
-on s.
+left join {{ ref('cmg_uwash_annotations') }} as cua
+on uc.code = cua.searched_code
