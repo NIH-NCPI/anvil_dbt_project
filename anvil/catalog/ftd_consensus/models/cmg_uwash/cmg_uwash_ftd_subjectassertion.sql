05c139978e950667023f4c5525725469e00e6d2a
yelenacox <yelenacox@gmail.com>
2025-11-07 21:33:13 +0000

    Obsolete codes not present in output

--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion.sql
@@ -9,7 +9,7 @@ with unioned_codes as (
     'disease' as assertion_type
   from {{ ref('cmg_uwash_stg_subject') }}
  
- union all
+ union 
  
  select 
     subject_id,
@@ -20,7 +20,23 @@ with unioned_codes as (
   from {{ ref('cmg_uwash_stg_subject') }}
      ),
      
-mapped_codes as (
+all_codes as (select
+    uc.subject_id as subject_id,
+    uc.value_display as value_display,
+    CASE 
+        WHEN cua.response_code NOT LIKE '%:%' THEN concat('OMIM:', cua.response_code)
+        ELSE cua.response_code
+    END as code,
+    cua.display as display,
+    null as assertion_type,
+    NULL as system
+    from unioned_codes as uc
+    join {{ ref('cmg_uwash_annotations') }} as cua
+    on uc.code = cua.searched_code
+         
+         
+    union
+    
     select
     subject_id,
     uc.value_display::text as "value_display",
@@ -30,54 +46,13 @@ mapped_codes as (
         WHEN 'CMG/phenotype_description' THEN 'phenotypic_feature'
         WHEN 'CMG/disease_description' THEN 'disease'
     END::text as assertion_type,
-    cm."code system" as "system"
+    cm."code system" as system
     from unioned_codes as uc
     left join {{ ref('cmg_uwash_condition_mappings') }} as cm
     on lower(uc.display) = lower(cm."local code")
-    
-    union all
-    
-    select 
-    subject_id,
-    value_display,
-    code,
-    display,
-    assertion_type,
-    NULL as "system"
-    from unioned_codes as uc
-),
-    
-annotated_codes as (
-    select 
-    mc.subject_id as subject_id,
-    mc.value_display as value_display,
-    mc.code as code,
-    cua.display as display,
-    CASE 
-        WHEN "searched_code" ILIKE '%HP%' THEN 'phenotypic_feature'
-        WHEN "searched_code" ILIKE '%OMIM%' THEN 'disease'
-    END::text as assertion_type,
-    mc.system
-    from mapped_codes as mc
-    join {{ ref('cmg_uwash_annotations') }} as cua
-    on mc.code = cua.searched_code
-    ),
-    
-all_unioned_codes as (
-    select distinct
-        mc.subject_id,
-        mc.value_display,
-        mc.code,
-        coalesce(ac.display, mc.display) as display,
-        coalesce(mc.assertion_type, ac.assertion_type) as assertion_type,
-        mc.system
-    from mapped_codes mc
-    left join annotated_codes as ac
-        on mc.subject_id = ac.subject_id
-        and mc.code = ac.code
 )
-   
-select 
+  
+ select 
   assertion_type,
   NULL as age_at_assertion,
   NULL as age_at_event,
@@ -109,8 +84,9 @@ select
   NULL as value_units_display,
     {{ generate_global_id(prefix='ap',descriptor=['consent_id'], study_id='phs000693') }}::text as "has_access_policy",
     {{ generate_global_id(prefix='sa',descriptor=['subject_id', 'code'], study_id='phs000693') }}::text as "id",
-    {{ generate_global_id(prefix='sb',descriptor=['subject_id', 'consent_id'], study_id='phs000693') }}::text as "subject_id"
+    subject_id
+--     { { generate_global_id(prefix='sb',descriptor=['subject_id', 'consent_id'], study_id='phs000693') }}::text as "subject_id"
 from {{ ref('cmg_uwash_stg_subject') }} as s
-left join all_unioned_codes as auc
+left join all_codes as alc
 using(subject_id)
 where code is not null
