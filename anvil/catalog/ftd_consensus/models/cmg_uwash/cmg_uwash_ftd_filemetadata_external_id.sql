18d72a47f5cf39f25277834d7f0fb51daac7b3ec
yelenacox <yelenacox@gmail.com>
2025-11-13 20:20:30 +0000

    Updated filemetadata_external_id

--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_filemetadata_external_id.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_filemetadata_external_id.sql
@@ -1,6 +1,20 @@
 {{ config(materialized='table', schema='cmg_uwash_data') }}
 
+with unioned_names as (
+        select
+            distinct 
+            name as "filename"
+        from {{ ref('cmg_uwash_stg_file_inventory') }}
+    
+        union all
+   
+        select
+            distinct 
+            sequencing_id as "filename"
+        from {{ ref('cmg_uwash_stg_sequencing') }}
+)
+
 select 
-  {{ generate_global_id(prefix='fd',descriptor=['name'], study_id='phs000693') }}::text as "filemetadata_id",
-  name::text as "external_id"
-from {{ ref('cmg_uwash_stg_file_inventory') }} as fi
+  {{ generate_global_id(prefix='fd',descriptor=['filename'], study_id='phs000693') }}::text as "filemetadata_id",
+  filename::text as "external_id"
+from unioned_names as un
