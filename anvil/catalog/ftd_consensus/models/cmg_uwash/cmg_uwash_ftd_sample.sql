169fc979a69968f16842ebafa6733dfe28b54401
yelenacox <yelenacox@gmail.com>
2025-11-14 16:46:49 +0000

    Updated sample to include analyte_type

--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_sample.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_sample.sql
@@ -2,12 +2,16 @@
 
 select 
  NULL::text as "parent_sample",
- sample_source::text as "sample_type",
+ coalesce(sm.sample_source, st.curie)::text as "sample_type",
  NULL::text as "availablity_status",
  NULL::text as "quantity_number",
  NULL::text as "quantity_units",
     {{ generate_global_id(prefix='ap',descriptor=['consent_id'], study_id='phs000693') }}::text as "has_access_policy",
     {{ generate_global_id(prefix='sm',descriptor=['subject_id','sample_id'], study_id='phs000693') }}::text as "id",
     {{ generate_global_id(prefix='sb',descriptor=['subject_id'], study_id='phs000693') }}::text as "subject_id",
-    NULL::text as "biospecimen_collection_id"
-from {{ ref('cmg_uwash_stg_sample') }} as s
+ NULL::text as "biospecimen_collection_id"
+from {{ ref('cmg_uwash_stg_sample') }} as sm
+left join {{ ref('cmg_uwash_stg_sequencing') }} as seq
+using(sample_id)
+left join {{ ref('sm_sample_type') }} as st
+on seq.analyte_type = st.src_format
