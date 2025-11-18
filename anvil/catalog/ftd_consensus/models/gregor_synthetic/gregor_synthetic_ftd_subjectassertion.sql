7583fa8e2becd2347af83932d822a8720c10cc5d
Yelena Cox <yelenacox@gmail.com>
2025-11-18 15:10:54 -0600

    Search-dragon annotations join for subjectassertion

--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_subjectassertion.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_subjectassertion.sql
@@ -4,23 +4,11 @@
         select 
         participant_id,
         consent_code,
-        phenotype.onset_age_range::text as "age_at_event",
-        CASE 
-            WHEN UPPER(phenotype.ontology) = 'HPO' THEN 'HP'
-            WHEN UPPER(phenotype.ontology) = 'MONDO' THEN 'MONDO'
-            WHEN UPPER(phenotype.ontology) = 'MAXO' THEN 'MAXO'
-            WHEN UPPER(phenotype.ontology) = 'NCIT' THEN 'NCIT'
-            WHEN UPPER(phenotype.ontology) = 'SNOMED CT' THEN 'SNOMED'
-            WHEN UPPER(phenotype.ontology) = 'SYMP' THEN 'SYMP'
-            WHEN UPPER(phenotype.ontology) = 'LOINC' THEN 'LOINC'
-            WHEN UPPER(phenotype.ontology) = 'MEDDRA' THEN 'MEDDRA'
-            WHEN UPPER(phenotype.ontology) = 'MESH' THEN 'MESH'
-            WHEN UPPER(phenotype.ontology) = 'UCUM' THEN 'UCUM'
-            WHEN UPPER(phenotype.ontology) = 'OMIT' THEN 'OMIT'
-            WHEN UPPER(phenotype.ontology) = 'OMIM' THEN 'MIM'
-            WHEN UPPER(phenotype.ontology) = 'CDCREC' THEN 'CDCREC'
-            ELSE phenotype.ontology
-        END || ':' || SPLIT_PART(phenotype.term_id, ':', 2)::text as "code",
+        CASE phenotype.onset_age_range
+            WHEN 'NA' THEN NULL
+            ELSE phenotype.onset_age_range
+        END::text as "age_at_event",
+        REPLACE(term_id, '?', '')::text as "code",
         CASE 
             WHEN LOWER(phenotype.presence) = 'present' THEN 'LA9633-4'
             WHEN LOWER(phenotype.presence) = 'absent' THEN 'LA9634-2'
@@ -40,12 +28,12 @@
     )
 
         select 
-        NULL::text as "assertion_type",
+        'phenotypic_feature'::text as "assertion_type",
         NULL::text as "age_at_assertion",
         age_at_event,
         NULL::text as "age_at_resolution",
-        code,
-        NULL::text as "display",
+        ga.response_code as "code",
+        ga.display as "display",
         value_code,
         value_display,
         value_number,
@@ -55,4 +43,6 @@
        {{ generate_global_id(prefix='sb',descriptor=['participant_id', 'consent_code'], study_id='gregor_synthetic') }}::text as "Subject_id",
        {{ generate_global_id(prefix='sa',descriptor=['participant_id', 'code'], study_id='gregor_synthetic') }}::text as "id"
        
-       from source
+       from source as s
+       left join {{ ref('gregor_synthetic_annotations') }} as ga
+       on lower(s.code) = lower(ga.searched_code)
