529e84e84415da5f17e012a8563893e3cac94e8e
Yelena Cox <yelenacox@gmail.com>
2025-11-18 15:22:34 -0600

    Modifying id in subjectassertion_external_id

--- anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_subjectassertion_external_id.sql
+++ anvil/catalog/ftd_consensus/models/gregor_synthetic/gregor_synthetic_ftd_subjectassertion_external_id.sql
@@ -10,7 +10,7 @@
     )
 
         select 
-        {{ generate_global_id(prefix='sa',descriptor=['participant_id', 'code'], study_id='gregor_synthetic') }}::text as "SubjectAssertion_id",
+        {{ generate_global_id(prefix='sa',descriptor=['participant_id','code'], study_id='gregor_synthetic') }}::text as "SubjectAssertion_id",
         participant_id::text as "external_id"
 
     from source as s
