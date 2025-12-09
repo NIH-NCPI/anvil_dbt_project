42060b9241cfd150d9a46beaecf2aab568bdc275
Yelena Cox <107718499+yelenacox@users.noreply.github.com>
2025-12-08 21:24:50 -0600

    Update cmg_uwash_ftd_demographics_source_data.sql

--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_demographics_source_data.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_demographics_source_data.sql
@@ -2,7 +2,7 @@
 
 select 
   {{ generate_global_id(prefix='st',descriptor=['dbgap_study_id'], study_id='phs000693') }}::text as "study_id",
-  {{ generate_global_id(prefix='dm',descriptor=['subject_id'], study_id='phs000693') }}::text as "demographics_id",
+  {{ generate_global_id(prefix='dm',descriptor=['subject_id', 'consent_id'], study_id='phs000693') }}::text as "demographics_id",
   {{ generate_global_id(prefix='sd',descriptor=['project_id'], study_id='phs000693') }}::text as "source_data_id"
   
 from (select distinct subject_id, project_id, dbgap_study_id 
