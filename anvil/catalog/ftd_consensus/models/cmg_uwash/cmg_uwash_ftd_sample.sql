39a06135b3678e8fb6319bcde9b00bbf2037b308
Yelena Cox <107718499+yelenacox@users.noreply.github.com>
2025-12-15 14:15:43 -0600

    Update cmg_uwash_ftd_sample.sql

--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_sample.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_sample.sql
@@ -8,7 +8,7 @@ select
  NULL::text as "quantity_units",
     {{ generate_global_id(prefix='ap',descriptor=['consent_id'], study_id='phs000693') }}::text as "has_access_policy",
     {{ generate_global_id(prefix='sm',descriptor=['subject_id','sample_id'], study_id='phs000693') }}::text as "id",
-    {{ generate_global_id(prefix='sb',descriptor=['subject_id'], study_id='phs000693') }}::text as "subject_id",
+    {{ generate_global_id(prefix='sb',descriptor=['subject_id','consent_id'], study_id='phs000693') }}::text as "subject_id",
  NULL::text as "biospecimen_collection_id"
 from {{ ref('cmg_uwash_stg_sample') }} as sm
 left join {{ ref('cmg_uwash_stg_sequencing') }} as seq
