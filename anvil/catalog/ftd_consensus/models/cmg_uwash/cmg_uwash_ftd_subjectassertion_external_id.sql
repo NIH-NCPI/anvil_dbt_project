6a3f4e256b883051d4055360a99a6ce850b131a3
yelenacox <yelenacox@gmail.com>
2026-01-16 19:16:59 +0000

    Updating subjectassertion_external_id and subjectassertion_source_data

--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion_external_id.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion_external_id.sql
@@ -98,22 +98,47 @@ all_conditions as (
     system
     from all_codes as ac
    ),
-   
-all_formatted_codes as (
-  select
+     
+ all_formatted_codes as (select distinct 
+  assertion_type,
+  NULL as age_at_assertion,
+  NULL as age_at_event,
+  NULL as age_at_resolution,
   CASE
       WHEN code NOT LIKE '%:%' AND system ILIKE '%omim%' THEN concat('OMIM:', code)
       WHEN code NOT LIKE '%:%' AND system ILIKE '%snomed%' THEN concat('SNOMED:', code)
       ELSE code
   END as code,
-  subject_id
+  display,
+  CASE 
+        WHEN LOWER(affected_status) = 'affected' THEN 'LA9633-4'
+        WHEN LOWER(affected_status) = 'unaffected' THEN 'LA9634-2'
+        WHEN LOWER(affected_status) = 'unknown' THEN 'LA4489-6'
+        WHEN LOWER(affected_status) = 'possibly affected' THEN 'LA15097-1'
+        WHEN affected_status is null THEN 'LA4489-6'
+        ELSE CONCAT('FTD_FLAG: unhandled value_code: ', affected_status)
+  END::text as "value_code",
+  CASE 
+        WHEN LOWER(affected_status) = 'affected' THEN 'Present'
+        WHEN LOWER(affected_status) = 'unaffected' THEN 'Absent'
+        WHEN LOWER(affected_status) = 'unknown' THEN 'Unknown'
+        WHEN LOWER(affected_status) = 'possibly affected' THEN 'Possible'
+        WHEN affected_status is null THEN 'Unknown'
+        ELSE CONCAT('FTD_FLAG: unhandled value_code: ', affected_status)
+  END::text as "value_display",
+  NULL as value_number,
+  NULL as value_units,
+  NULL as value_units_display,
+    {{ generate_global_id(prefix='ap',descriptor=['consent_id'], study_id='phs000693') }}::text as "has_access_policy",
+    {{ generate_global_id(prefix='sa',descriptor=['subject_id', 'code'], study_id='phs000693') }}::text as "id",
+    {{ generate_global_id(prefix='sb',descriptor=['subject_id', 'consent_id'], study_id='phs000693') }}::text as "subject_id"
 from {{ ref('cmg_uwash_stg_subject') }} as s
 left join all_conditions as alc
 using(subject_id)
-    )
-        
+where code is not null)
+
 select distinct
-  {{ generate_global_id(prefix='sa',descriptor=['subject_id', 'code'], study_id='phs000693') }}::text as "subjectassertion_id",
+  id::text as "subjectassertion_id",
   subject_id::text as "external_id"
 from all_formatted_codes as afc
 where code is not null
--- anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion_source_data.sql
+++ anvil/catalog/ftd_consensus/models/cmg_uwash/cmg_uwash_ftd_subjectassertion_source_data.sql
@@ -3,27 +3,33 @@
 with unioned_codes as (
     select 
     subject_id,
+    affected_status as "value_display",
     UNNEST(IFNULL(SPLIT(REPLACE(disease_id, ';', ''), '|'),[NULL])) as code,
     disease_description as "display",
-  from (select subject_id, disease_id, disease_description from {{ ref('cmg_uwash_stg_subject') }})
+    'disease' as assertion_type
+  from {{ ref('cmg_uwash_stg_subject') }}
  
  union 
  
  select 
     subject_id,
+    affected_status as "value_display",
     UNNEST(IFNULL(SPLIT(REPLACE(hpo_present, ';', ''), '|'),[NULL])) as code,
     phenotype_description as "display",
-  from (select subject_id, hpo_present, phenotype_description from {{ ref('cmg_uwash_stg_subject') }})
+    'phenotypic_feature' as assertion_type
+  from {{ ref('cmg_uwash_stg_subject') }}
      ),
              
 all_codes as (
     select
     uc.subject_id as subject_id,
+    uc.value_display as value_display,
     CASE 
         WHEN cua.response_code NOT LIKE '%:%' THEN concat('OMIM:', cua.response_code)
         ELSE cua.response_code
     END as code,
     cua.display as display,
+    uc.assertion_type,
     NULL as system
     from unioned_codes as uc
     join {{ ref('cmg_uwash_annotations') }} as cua
@@ -33,8 +39,13 @@ all_codes as (
     
     select
     subject_id,
+    uc.value_display::text as "value_display",
     cm.code as code,
     cm.display as display,
+    CASE "local code system"
+        WHEN 'CMG/phenotype_description' THEN 'phenotypic_feature'
+        WHEN 'CMG/disease_description' THEN 'disease'
+    END::text as assertion_type,
     cm."code system" as system
     from unioned_codes as uc
     left join {{ ref('cmg_uwash_split_text_mappings_var') }} as st
@@ -46,8 +57,13 @@ all_codes as (
         
     select 
     subject_id,
+    s.value_display,
     cm.code,
     cm.display,
+    CASE "local code system"
+        WHEN 'CMG/phenotype_description' THEN 'phenotypic_feature'
+        WHEN 'CMG/disease_description' THEN 'disease'
+    END::text as assertion_type,
     cm."code system" as system
     from unioned_codes as s
     left join {{ ref('cmg_uwash_condition_mappings') }} as cm
@@ -58,8 +74,13 @@ all_codes as (
 all_conditions as (
     select 
     subject_id,
+    s.value_display,
     cm.code,
     cm.display,
+    CASE "local code system"
+        WHEN 'CMG/phenotype_description' THEN 'phenotypic_feature'
+        WHEN 'CMG/disease_description' THEN 'disease'
+    END::text as assertion_type,
     cm."code system" as system
     from unioned_codes as s
     left join {{ ref('cmg_uwash_condition_mappings') }} as cm
@@ -70,26 +91,54 @@ all_conditions as (
     
     select 
     subject_id,
+    value_display,
     code,
     display,
+    assertion_type,
     system
     from all_codes as ac
    ),
-   
-all_formatted_codes as (
-    select
-    subject_id, 
-    CASE
+     
+ all_formatted_codes as (select distinct 
+  assertion_type,
+  NULL as age_at_assertion,
+  NULL as age_at_event,
+  NULL as age_at_resolution,
+  CASE
       WHEN code NOT LIKE '%:%' AND system ILIKE '%omim%' THEN concat('OMIM:', code)
       WHEN code NOT LIKE '%:%' AND system ILIKE '%snomed%' THEN concat('SNOMED:', code)
       ELSE code
-    END as code,
-    display
-    from all_conditions as a
-    )
+  END as code,
+  display,
+  CASE 
+        WHEN LOWER(affected_status) = 'affected' THEN 'LA9633-4'
+        WHEN LOWER(affected_status) = 'unaffected' THEN 'LA9634-2'
+        WHEN LOWER(affected_status) = 'unknown' THEN 'LA4489-6'
+        WHEN LOWER(affected_status) = 'possibly affected' THEN 'LA15097-1'
+        WHEN affected_status is null THEN 'LA4489-6'
+        ELSE CONCAT('FTD_FLAG: unhandled value_code: ', affected_status)
+  END::text as "value_code",
+  CASE 
+        WHEN LOWER(affected_status) = 'affected' THEN 'Present'
+        WHEN LOWER(affected_status) = 'unaffected' THEN 'Absent'
+        WHEN LOWER(affected_status) = 'unknown' THEN 'Unknown'
+        WHEN LOWER(affected_status) = 'possibly affected' THEN 'Possible'
+        WHEN affected_status is null THEN 'Unknown'
+        ELSE CONCAT('FTD_FLAG: unhandled value_code: ', affected_status)
+  END::text as "value_display",
+  NULL as value_number,
+  NULL as value_units,
+  NULL as value_units_display,
+    {{ generate_global_id(prefix='ap',descriptor=['consent_id'], study_id='phs000693') }}::text as "has_access_policy",
+    {{ generate_global_id(prefix='sa',descriptor=['subject_id', 'code'], study_id='phs000693') }}::text as "id",
+   subject_id
+from {{ ref('cmg_uwash_stg_subject') }} as s
+left join all_conditions as alc
+using(subject_id)
+where code is not null)
 
 select distinct
-  {{ generate_global_id(prefix='sa',descriptor=['subject_id', 'code'], study_id='phs000693') }}::text as "subjectassertion_id",
+afc.id::text as "subjectassertion_id",
     {{ generate_global_id(prefix='sd',descriptor=['project_id'], study_id='phs000693') }}::text as "source_data_id"
 from {{ ref('cmg_uwash_stg_subject') }} as subject
 left join all_formatted_codes as afc
