57d6fc3063f327f199c1d5cf4877a5d6abaff1c5
yelenacox <yelenacox@gmail.com>
2026-01-28 21:59:41 +0000

    Family (unharmonized)

--- anvil/catalog/ftd_consensus/models/alscompute/alscompute_ftd_family_family_relationships.sql
+++ anvil/catalog/ftd_consensus/models/alscompute/alscompute_ftd_family_family_relationships.sql
@@ -1,11 +1,5 @@
 {{ config(materialized='table', schema='alscompute_data') }}
 
 select 
-  {{ generate_global_id(prefix='',descriptor=[''], study_id='alscompute') }}::text as "family_id",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='alscompute') }}::text as "family_relationships_id"
-from {{ ref('alscompute_stg_anvil_dataset') }} as anvil_dataset
-join {{ ref('alscompute_stg_file_inventory') }} as file_inventory
-on harmonized_genotypes.file_inventory_id = file_inventory.file_inventory_id  join {{ ref('alscompute_stg_harmonized_genotypes') }} as harmonized_genotypes
-on file_inventory.file_inventory_id = harmonized_genotypes.file_inventory_id  join {{ ref('alscompute_stg_sample') }} as sample
-on  
-
+NULL::text as "family_id",
+NULL::text as "family_relationships_id"
--- anvil/catalog/ftd_consensus/models/alscompute/alscompute_ftd_familymember.sql
+++ anvil/catalog/ftd_consensus/models/alscompute/alscompute_ftd_familymember.sql
@@ -1,14 +1,8 @@
 {{ config(materialized='table', schema='alscompute_data') }}
 
 select 
-  {{ generate_global_id(prefix='',descriptor=[''], study_id='alscompute') }}::text as "family_member",
-  GEN_UNKNOWN.family_role::text as "family_role",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='alscompute') }}::text as "has_access_policy",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='alscompute') }}::text as "id",
-    {{ generate_global_id(prefix='',descriptor=[''], study_id='alscompute') }}::text as "family_id"
-from {{ ref('alscompute_stg_anvil_dataset') }} as anvil_dataset
-join {{ ref('alscompute_stg_file_inventory') }} as file_inventory
-on harmonized_genotypes.file_inventory_id = file_inventory.file_inventory_id  join {{ ref('alscompute_stg_harmonized_genotypes') }} as harmonized_genotypes
-on file_inventory.file_inventory_id = harmonized_genotypes.file_inventory_id  join {{ ref('alscompute_stg_sample') }} as sample
-on  
-
+NULL::text as "family_member",
+NULL::text as "family_role",
+NULL::text as "has_access_policy",
+NULL::text as "id",
+NULL::text as "family_id"
